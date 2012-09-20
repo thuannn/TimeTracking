03600d0b842387e7ab7bbf504585b2e98e308fd3
Thuan Bootcamp <thuannn@gmail.com>
2012-09-20 21:33:01 +0200

    Task in progress : time logging

---
+++ TimeTracking/src/com/lemania/timetracking/client/uihandler/TimeInputUiHandler.java
@@ -0,0 +1,12 @@
+package com.lemania.timetracking.client.uihandler;
+
+import com.gwtplatform.mvp.client.UiHandlers;
+import com.lemania.timetracking.shared.ProfessorProxy;
+
+
+public interface TimeInputUiHandler extends UiHandlers {
+		
+	void professorSelected(ProfessorProxy prof);
+		
+	void loadCoursesBySchool(String ecoleId);
+}
