6d77a6ad027179f38fb6034a17476962ded8efb1
thuannn <thuannn@ThuanL.lemania-admin.local>
2012-09-21 15:22:21 +0200

    Task in progress : user authentication

--- TimeTracking/src/com/lemania/timetracking/client/presenter/HomePresenter.java
+++ TimeTracking/src/com/lemania/timetracking/client/presenter/HomePresenter.java
@@ -1,20 +1,24 @@
 package com.lemania.timetracking.client.presenter;
 
+import com.gwtplatform.mvp.client.HasUiHandlers;
 import com.gwtplatform.mvp.client.Presenter;
 import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.lemania.timetracking.client.place.NameTokens;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.proxy.RevealContentEvent;
 import com.lemania.timetracking.client.presenter.MainPagePresenter;
+import com.lemania.timetracking.client.uihandler.HomeUiHandler;
 
-public class HomePresenter extends
-		Presenter<HomePresenter.MyView, HomePresenter.MyProxy> {
+public class HomePresenter 
+		extends Presenter<HomePresenter.MyView, HomePresenter.MyProxy> 
+		implements HomeUiHandler {
 
-	public interface MyView extends View {
+	public interface MyView extends View, HasUiHandlers<HomeUiHandler> {
 	}
 
 	@ProxyCodeSplit
@@ -36,5 +40,19 @@ public class HomePresenter extends
 	@Override
 	protected void onBind() {
 		super.onBind();
+		
+		// Thuan
+		getView().setUiHandlers(this);
+	}
+
+	@Override
+	public void authenticateUser(String userName, String password) {
+		if (userName.equals("") || password.equals("")) {
+			Window.alert("Veuillez introduire votre nom d'utilisateur et mot de passe !");
+			return;
+		}
+		
+		// Validate user name and password here
+		
 	}
 }
--- TimeTracking/src/com/lemania/timetracking/client/view/HomeView.java
+++ TimeTracking/src/com/lemania/timetracking/client/view/HomeView.java
@@ -1,15 +1,25 @@
 package com.lemania.timetracking.client.view;
 
-import com.gwtplatform.mvp.client.ViewImpl;
+import com.gwtplatform.mvp.client.ViewWithUiHandlers;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.lemania.timetracking.client.presenter.HomePresenter;
+import com.lemania.timetracking.client.uihandler.HomeUiHandler;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 
-public class HomeView extends ViewImpl implements HomePresenter.MyView {
+public class HomeView extends ViewWithUiHandlers<HomeUiHandler> implements HomePresenter.MyView {
 	
 	private static HomeViewUiBinder uiBinder = GWT.create(HomeViewUiBinder.class);
+	@UiField TextBox txtUserName;
+	@UiField Button cmdLogin;
+	@UiField PasswordTextBox txtPassword;
 	interface HomeViewUiBinder extends UiBinder<Widget, HomeView> {}
 
 	private final Widget widget;
@@ -25,5 +35,11 @@ public class HomeView extends ViewImpl implements HomePresenter.MyView {
 	@Override
 	public Widget asWidget() {
 		return widget;
+	}
+	
+	@UiHandler("cmdLogin")
+	public void onCmdLoginClicked(ClickEvent event){
+		if (this.getUiHandlers() != null)
+			this.getUiHandlers().authenticateUser(txtUserName.getText(), txtPassword.getText());
 	}
 }
